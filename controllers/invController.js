91c6553346ca6756f6c8ee54f5bf5646b88c0ab1
wdd130 <wdd13000@gmail.com>
2025-09-24 08:49:45 +0000

    serrver

--- controllers/invController.js
+++ controllers/invController.js
@@ -7,17 +7,28 @@ const invCont = {}
  *  Build inventory by classification view
  * ************************** */
 invCont.buildByClassificationId = async function (req, res, next) {
-  const classification_id = req.params.classificationId
-  const data = await invModel.getInventoryByClassificationId(classification_id)
-  const grid = await utilities.buildClassificationGrid(data)
-  let nav = await utilities.getNav()
- 
-  const className = data[0].classification_name
-  res.render("./inventory/classification", {
-    title: className + " vehicles",
-    nav,
-    grid,
-  })
+  try {
+    const classification_id = req.params.classificationId
+    const data = await invModel.getInventoryByClassificationId(classification_id)
+    if (!data || data.length === 0) {
+      return res.status(404).render("inventory/noData", {
+        title: "No Vehicles Found",
+        message: "Sorry, no vehicles were found for this classification.",
+        nav: await utilities.getNav()
+      })
+    }
+    const grid = await utilities.buildClassificationGrid(data)
+    let nav = await utilities.getNav()
+    const className = data[0].classification_name
+    res.render("./inventory/classification", {
+      title: className + " vehicles",
+      nav,
+      grid,
+    })
+  } catch (error) {
+    console.error("Error in buildByClassificationId:", error)
+    next(error)
+  }
 }
 
 /* ***************************
